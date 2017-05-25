7c5bfca425b3e25964bf9a06f9ea446358381555
Andrea Gallo <agallo@jbknowledge.com>
2017-05-25 11:50:40 -0300

    Connect with redux

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component{
+  render() {
+    return(
+      <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do ena aliqua. Ut enim ad minim venie irure dolor in reprehenderit in voluptate vel.</p>
+      </div>
+    );
+  }
+}
+
+export default AboutPage;
---
+++ src/components/common/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { IndexLink, Link } from 'react-router';
+
+const Header = () => {
+    return (
+      <nav>
+        <IndexLink to="/" activeClassName="active">Home</IndexLink>
+        {"|"}
+        <Link to="about" activeClassName="active">About</Link>
+        {"|"}
+        <Link to="courses" activeClassName="active">Courses</Link>
+      </nav>
+    );
+};
+
+export default Header;
